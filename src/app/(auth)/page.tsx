cfd1bac2a30c9b07e289eb244af6c1d10ff728d3
Josh Mackus <josh.mackus@ceridian.com>
2024-09-16 16:03:55 +1000

    fix bg box suspense

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,17 +1,13 @@
 import AddTransactionCard from "@/components/cards/AddTransactionCard";
 import OverviewCard from "@/components/cards/OverviewCard";
 import TransactionsTable from "@/components/cards/TransactionsTable";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Suspense } from "react";
 
 export default function Dashboard() {
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       <AddTransactionCard />
       <OverviewCard />
-      <Suspense fallback={<Skeleton className="h-[200px] w-full" />}>
-        <TransactionsTable />
-      </Suspense>
+      <TransactionsTable />
     </div>
   );
 }
