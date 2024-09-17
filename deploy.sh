70b4f56f7d1972390675d0c09bc086680e118432
Josh Mackus <josh.mackus@ceridian.com>
2024-09-17 23:50:34 +1000

    test

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,12 @@ if ! command -v supabase &> /dev/null; then
     npm install -g supabase
 fi
 
+# Authenticate Supabase CLI
+supabase login --access-token "$SUPABASE_ACCESS_TOKEN"
+
+# Link to the Supabase project
+supabase link --project-ref "$SUPABASE_PROJECT_ID"
+
 # Run database migrations
 supabase db push
 
