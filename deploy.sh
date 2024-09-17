0ab0163baa99f2f48822965a65502ed2bf1f465c
Josh Mackus <josh.mackus@ceridian.com>
2024-09-17 21:49:32 +1000

    test

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# Replace placeholders in config.toml with actual secrets
-sed -i "s|SUPABASE_JWT_SECRET|${SUPABASE_JWT_SECRET}|g" supabase/config.toml
-
-# Run database migrations
-npx supabase db push
-
 # Build the Next.js app
 npm run build
