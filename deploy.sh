5b5a5981ffdef978352b2146e2e5b6f9546d021c
Josh Mackus <josh.mackus@ceridian.com>
2024-09-17 20:30:22 +1000

    test

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-# Install Supabase CLI
-npm install -g supabase
-
-# Set the JWT secret using an environment variable
-export SUPABASE_AUTH_JWT_SECRET="$SUPABASE_JWT_SECRET"
-
 # Run database migrations
-supabase db push
+npx supabase db push
 
 # Build the Next.js app
 npm run build
