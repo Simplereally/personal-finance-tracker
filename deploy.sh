0ae48b5548a6fc02b55e9f5bacab6baccbfdcd78
Josh Mackus <josh.mackus@ceridian.com>
2024-09-17 20:21:57 +1000

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Replace placeholders in config.toml with actual secrets
+sed -i 's/SUPABASE_JWT_SECRET/'"$SUPABASE_JWT_SECRET"'/' supabase/config.toml
+    
+
 # Run database migrations
 npx supabase db push
 
