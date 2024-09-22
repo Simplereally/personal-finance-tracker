6e61033cbda2af55f5710b9d5c5e98ceac750807
Josh Mackus <josh.mackus@ceridian.com>
2024-09-22 12:45:25 +1000

    pre-margin fix

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -2,7 +2,7 @@ import DashboardContent from "@/components/DashboardContent";
 
 export default function Dashboard() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto">
       <h1 className="mb-8 text-3xl font-bold text-primary">Your Dashboard</h1>
       <DashboardContent />
     </div>
